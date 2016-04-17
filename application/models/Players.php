2ca1a95d101b7e49d91dac814dc407ee6efcc37f
AHartleb <maxwellfreemanorr@gmail.com>
2016-04-17 09:30:11 -0700

    Reg Func
    Reg Func

--- application/models/Players.php
+++ application/models/Players.php
@@ -4,12 +4,36 @@
 		function __construct(){
 			parent::__construct();
 		}
-
+		//returns array of all players
 		function all() 
 		{
 			$this->db->order_by('Player', 'Peanuts');
 			$query = $this->db->get('Players');
 			return $query->result_array();
 		}
+		//returns array of specific player
+		function getPlayer($player)
+		{
+			$st="Player = '".$player."'";
+			$this->db->where($st, NULL, FALSE);  
+			$player_array = $this->db->get('players')->result_array();
+			return $player_array;
+
+		}
+		//check credentials
+		function logon($user, $password)
+		{
+			$this->db->order_by('Player', 'Password');
+			$st="Player='".$user."' AND Password = '".$password."'";
+  			$this->db->where($st, NULL, FALSE);  
+			$query = $this->db->get('Players');
+			if(sizeof($query->result_array()) > 0){
+				return TRUE;
+			}
+			else{
+				return FALSE;
+			}
+		}
+	
 
 	}
--- application/views/_collectioncell.php
+++ application/views/_collectioncell.php
@@ -3,8 +3,7 @@
 
 ?>
 
-<a href="/data/{Piece}.jpeg" data-lightbox="gallery"
-	data-title="{Piece} ... Player {Player}">
+
 	<img src="/data/{Piece}.jpeg" />
 
 
